a8cb437d3558417372c6a902ea4243f6e587ad53
Rishabh Sharma <r08s2001@gmail.com>
2024-09-04 20:16:38 +0530

    Initial Commit

---
+++ src/components/LandingPageMessage.jsx
@@ -0,0 +1,25 @@
+import logoLightBg from "../assets/logo-dark-bg.jpg";
+
+const LandingPageMessage = () => {
+  return (
+    <div className="flex flex-col m-auto items-center w-[50vw] gap-20 font-bold text-3xl text-center">
+      <div>
+        <a href="">
+          <img
+            className="h-[40vh] hover:shadow-2xl rounded-full hover:shadow-neutral-600 transition-all duration-400 object-cover"
+            src={logoLightBg}
+            alt="Vite logo"
+          />
+        </a>
+      </div>
+      <h1>
+        🌟 We are crafting something extraordinary just for you. Stay tuned as
+        we prepare to launch a world of innovation and excitement. Check back
+        soon for the grand unveiling!
+      </h1>
+      <h1>🚀 Coming Soon: Infinity Toytronics! </h1>
+    </div>
+  );
+};
+
+export default LandingPageMessage;
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,11 @@
+import LandingPageMessage from "../components/LandingPageMessage";
+
+const LandingPage = () => {
+  return (
+    <div className="flex h-screen m-auto bg-neutral-800 text-neutral-300 ">
+      <LandingPageMessage />
+    </div>
+  );
+};
+
+export default LandingPage;
